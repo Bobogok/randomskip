ddb691e660d0bc124bfe0eda12a0caa2d867cf6e
Ivan M <40141225+Bobogok@users.noreply.github.com>
2021-03-26 22:48:35 +0400

    update script
    обновил, чтобы все заработало с html

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function luck(){
-    var luck = ["<h1>не повезло</h1>", "<h2>повезло</h2>", "<h2>повезло</h2", "<h2>повезло</h2", "<h2>повезло</h2", "<h2>повезло</h2"];
+    var luck = ["<h2>не повезло</h2>", "<h2>повезло</h2>", "<h2>повезло</h2", "<h2>повезло</h2", "<h2>повезло</h2", "<h2>повезло</h2"];
 
     var random = Math.floor(Math.random() * luck.length);
 
     if (random == 0) {
-        var a = document.createElement("h2");
-        var text = document.createTextNode("паъхапъхпаъхапъх");
-        a.appendChild(text);
+        var text = '<h1>ЧЛЭН</h1>';
+        var cock = document.getElementById('new');
+        cock.innerHTML = text;
     };
     
     var your_luck = luck[random];
     var luckElement = document.getElementById("your_luck");
     luckElement.innerHTML = your_luck;
-    a.innerHTML;
 }
 window.onload = luck;
